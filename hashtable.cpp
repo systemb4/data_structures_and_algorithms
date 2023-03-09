d53a19f5e1a7ca821dbfd0345c60c1246e5972ef
systemb4 <lukasnitzsche@yahoo.com>
2023-03-09 03:08:18 -0600

    trying to build hashtable with linked list

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,5 +1,84 @@
 #include <iostream>
 
+#define HASHMAX 10
+
+class Node {
+    std::string name;
+    public:
+        Node *next;
+
+        Node() {
+            name = "Test";
+            next = NULL;
+        }
+
+        Node(std::string n_name) {
+            name = n_name;
+            next = NULL;
+        }
+
+        std::string getName() {
+            return name;
+        }
+};
+
+class LinkedList {
+    Node *head;
+    public:
+        LinkedList() {
+            head = NULL;
+        }
+
+        void addToEnd(std::string);
+        void printList();
+};
+
+LinkedList **hashArr;
+
+void LinkedList::addToEnd(std::string n_name) {
+    Node *newNode = new Node(n_name);
+
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    Node *tmp = head;
+    while(tmp->next != NULL) {
+        tmp = tmp->next;
+    }
+
+    tmp->next = newNode;
+}
+
+void LinkedList::printList() {
+    Node *tmp = head;
+    while(tmp->next != NULL) {
+        std::cout << tmp->getName() << std::endl;
+    }
+}
+
+int hashFunction(std::string name) {
+    int sum = 0;
+
+    for(int i = 0; name[i] != '\0'; i++) {
+        sum += name[i];
+    }
+
+    return sum % HASHMAX;
+}
+
+void hashSort(int hashNum, std::string name) {
+    hashArr[hashNum]->addToEnd(name);
+}
+
 int main() {
+    hashArr = new LinkedList*[HASHMAX];
+    for(int i = 0; i < HASHMAX; i++) {
+        hashArr[i] = new LinkedList;
+    }
+
+    hashArr[2]->addToEnd("Lukas");
+
     return 0;
 }
